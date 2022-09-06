ccf7de4a43d0ad34b48443a972c4e02e37a9dd6f
Wesley Oliveira <wesleyosantos91@gmail.com>
2022-09-06 15:14:46 -0300

    Update 01-create-profile.sh

--- init-scripts/01-create-profile.sh
+++ init-scripts/01-create-profile.sh
@@ -4,7 +4,7 @@ echo "########### Starting ##############"
 
 aws configure set aws_access_key_id default_access_key --profile localstack
 aws configure set aws_secret_access_key default_secret_key --profile localstack
-aws configure set region sa-east-1 --profile=localstack
+aws configure set region sa-east-1 --profile localstack
 
 echo "########### Listing profile #######"
-aws configure list --profile=localstack
+aws configure list --profile localstack
